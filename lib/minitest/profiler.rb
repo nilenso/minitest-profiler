1565469b3afadd2c3746d5dc51ba7e2eb3f77f2c
Timothy Andrew <mail@timothyandrew.net>
2014-05-15 14:45:40 +0530

    Load the Minitest `after_run` hook only when the module is included.

--- lib/minitest/profiler.rb
+++ lib/minitest/profiler.rb
@@ -18,17 +18,17 @@ module Minitest
         time_taken = end_time - @start_time
         @@minitest_profiler_report << {time: time_taken, name: @NAME}
       end
-    end
 
-    Minitest.after_run do
-     puts
-     puts "-----------------------------------------"
-     puts "Timing Information for the Last Test Run"
-     puts "-----------------------------------------"
-     puts
-     puts "Time, Test Name"
-     puts
-     @@minitest_profiler_report.sort_by { |timing| timing[:time] }.reverse.each { |timing| puts "#{timing[:time]}, #{timing[:name]}" }
+      Minitest.after_run do
+        puts
+        puts "-----------------------------------------"
+        puts "Timing Information for the Last Test Run"
+        puts "-----------------------------------------"
+        puts
+        puts "Time, Test Name"
+        puts
+        @@minitest_profiler_report.sort_by { |timing| timing[:time] }.reverse.each { |timing| puts "#{timing[:time]}, #{timing[:name]}" }
+      end
     end
   end
 end
